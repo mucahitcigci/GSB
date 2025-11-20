33f1f81150764e1cb9e506db29d8c146e2325ba4
Mücahit ÇIĞCİ <86369604+mucahitcigci@users.noreply.github.com>
2025-11-20 16:59:47 +0300

    Add files via upload

---
+++ webTasarim_3/JsUygulamalari/read.js
@@ -0,0 +1,10 @@
+console.log("merhaba");
+
+let not = 15;
+if (not >= 90) {
+  console.log("Pekiyi");
+} else if (not >= 70) {
+  console.log("İyi");
+} else {
+  console.log("Daha çok çalışmalısın");
+}
